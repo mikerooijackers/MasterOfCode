5b0cd51b64818d8914a30492ea6c577295694cde
TimHermens <timhermens@hotmail.com>
2015-04-16 14:12:35 +0200

    Annotation classes added

---
+++ MasterOfCode/Domein/src/main/java/Domein/AnnotationData.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domein;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Gebruiker
+ */
+public class AnnotationData {
+    private String name;
+    private String annotationName;
+    private List<AnnotationMethod> methods;
+
+    public AnnotationData() {
+        this.methods = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAnnotationName() {
+        return annotationName;
+    }
+
+    public void setAnnotationName(String annotationName) {
+        this.annotationName = annotationName;
+    }
+
+    public List<AnnotationMethod> getMethods() {
+        return methods;
+    }
+
+    public void setMethods(List<AnnotationMethod> methods) {
+        this.methods = methods;
+    }
+    
+    public void addMethod(AnnotationMethod method) {
+        this.methods.add(method);
+    }
+}
