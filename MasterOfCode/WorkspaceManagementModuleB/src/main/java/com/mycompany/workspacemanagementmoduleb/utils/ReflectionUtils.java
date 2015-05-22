31b82ad12cd25b414be709dc98cf524f0821735d
TimHermens <timhermens@hotmail.com>
2015-05-22 11:21:00 +0200

    updated module b

--- MasterOfCode/WorkspaceManagementModuleB/src/main/java/com/mycompany/workspacemanagementmoduleb/utils/ReflectionUtils.java
+++ MasterOfCode/WorkspaceManagementModuleB/src/main/java/com/mycompany/workspacemanagementmoduleb/utils/ReflectionUtils.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -22,19 +23,36 @@ import java.util.logging.Logger;
 import org.reflections.Reflections;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
+import org.testng.annotations.Test;
 
 /**
  *
  * @author Gebruiker
  */
 public class ReflectionUtils {
+    
+    private static List<String> testMethodNames = Arrays.asList("testName", "groups", "priority", "description" );
 
     public static List<AnnotationData> readAnnotationData(String path, Class<? extends Annotation>... annotations) {
         List<AnnotationData> annotationData = new ArrayList<>();
         File folder = new File(path);
         File[] files = folder.listFiles();
         for (File file : files) {
-            if (FileUtils.checkFileExtension(file, ".jar")) {
+            if (FileUtils.checkFileExtension(file, ".jar") && !file.getName().endsWith("-tests.jar")) {
+                annotationData = ReflectionUtils.readAnnotationData(file, annotations);
+                break;
+            }
+        }
+        
+        return annotationData;
+    }
+    
+    public static List<AnnotationData> readTestAnnotationData(String path, Class<? extends Annotation>... annotations) {
+        List<AnnotationData> annotationData = new ArrayList<>();
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            if (FileUtils.checkFileExtension(file, ".jar") && file.getName().endsWith("-tests.jar")) {
                 annotationData = ReflectionUtils.readAnnotationData(file, annotations);
                 break;
             }
@@ -54,7 +72,7 @@ public class ReflectionUtils {
             
             for (Class<? extends Annotation> an : annotations) {
                 Set<Class<?>> annotated = r.getTypesAnnotatedWith(an);
-
+                
                 Iterator<Class<?>> iterator = annotated.iterator();
                 while (iterator.hasNext()) {
 
@@ -65,6 +83,51 @@ public class ReflectionUtils {
                         Class<Annotation> type = (Class<Annotation>) annotation.annotationType();
                         AnnotationData data = new AnnotationData();
                         data.setName(foundClass.getName());
+                        
+                        data.setAnnotationName(type.getSimpleName());
+                        for (Method method : type.getDeclaredMethods()) {
+                            if (type.getSimpleName().equals("Test")) {
+                                if (testMethodNames.contains(method.getName())) {
+                                    data.addMethod(new AnnotationMethod(method.getName(), method.invoke(annotation, null)));
+                                }
+                            }
+                            else {
+                                data.addMethod(new AnnotationMethod(method.getName(), method.invoke(annotation, null)));
+                            }
+                        }
+                        annotationData.add(data);
+                    }
+                }
+            }
+        } catch (MalformedURLException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+            Logger.getLogger(ReflectionUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return annotationData;
+    }
+    
+    private static List<AnnotationData> readMethodAnnotationData(File file, Class<? extends Annotation>... annotations) {
+        List<AnnotationData> annotationData = new ArrayList<>();
+        Reflections r;
+
+        try {
+            URLClassLoader loader = new URLClassLoader(new java.net.URL[]{file.toURI().toURL()});
+
+            r = new Reflections(new ConfigurationBuilder().setUrls(ClasspathHelper.forClassLoader(loader)).addClassLoader(loader));
+            
+            for (Class<? extends Annotation> an : annotations) {
+                Set<Method> annotated = r.getMethodsAnnotatedWith(an);
+
+                Iterator<Method> iterator = annotated.iterator();
+                while (iterator.hasNext()) {
+
+                    Method foundMethod = iterator.next();
+
+                    for (Annotation annotation : foundMethod.getAnnotationsByType(an)) {
+                        
+                        Class<Annotation> type = (Class<Annotation>) annotation.annotationType();
+                        AnnotationData data = new AnnotationData();
+                        data.setName(foundMethod.getName());
                         
                         data.setAnnotationName(type.getSimpleName());
                         for (Method method : type.getDeclaredMethods()) {
---
+++ MasterOfCode/WorkspaceManagementModuleB/src/test/java/NewEmptyJUnitTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Domein.AnnotationData;
+import com.mycompany.workspacemanagementmoduleb.WorkspaceService;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Gebruiker
+ */
+public class NewEmptyJUnitTest {
+    
+    public NewEmptyJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @org.testng.annotations.Test
+    public void testReadAnnotationData() {
+        WorkspaceService service = new WorkspaceService();
+        String path = "C:\\assignments";
+        List<AnnotationData> readAssignmentMetaData = service.readAssignmentMetaData(path, 1L);
+        Assert.assertTrue(true);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
