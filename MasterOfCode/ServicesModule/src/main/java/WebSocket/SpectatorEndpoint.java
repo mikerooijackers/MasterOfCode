8f8410ee58aebdee42f79b78b69977470787e305
mjhjansen <mjh.jansen@student.fontys.nl>
2015-05-27 09:51:01 +0200

    Implemented SpectatorEndpoint (except for sendMessage method)

--- MasterOfCode/ServicesModule/src/main/java/WebSocket/SpectatorEndpoint.java
+++ MasterOfCode/ServicesModule/src/main/java/WebSocket/SpectatorEndpoint.java
@@ -9,6 +9,12 @@ import MessageUtils.MessageDecoder;
 import MessageUtils.MessageEncoder;
 import Service.CommunicationBean;
 import Sockets.Configurator;
+import Sockets.Messages.BaseMessage;
+import Sockets.Messages.DebugMessage;
+import Sockets.Messages.NewSessionConnectionMessage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.ConcurrencyManagement;
@@ -16,6 +22,7 @@ import javax.ejb.ConcurrencyManagementType;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 import javax.websocket.CloseReason;
+import javax.websocket.EncodeException;
 import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -42,6 +49,8 @@ public class SpectatorEndpoint {
     @Inject
     private CommunicationBean communicationBean;
 
+    private List<Session> sessions = new ArrayList();
+
     /**
      *
      * @param endpointConfig
@@ -54,16 +63,31 @@ public class SpectatorEndpoint {
 
     /**
      *
+     * @param session
      * @param message
-     * @return
      */
     @OnMessage
-    public String onMessage(String message) {
-        return null;
+    public void onMessage(final Session session, final BaseMessage message) {
+        if (message instanceof NewSessionConnectionMessage) {
+            this.addSession(session);
+        } else if (message instanceof DebugMessage) {
+        } else {
+            message.doAction(communicationBean);
+        }
+        System.out.println("Sessions size: " + sessions.size());
     }
-    
+
+    private void addSession(Session session) {
+        this.sessions.add(session);
+    }
+
+    // TODO: implement this method for all sessions
     public void sendMessage(Object message) {
-        
+//        try {
+//            sessions.get(username).getBasicRemote().sendObject(message);
+//        } catch (IOException | EncodeException ex) {
+//            Logger.getLogger(CompetitorEndPoint.class.getName()).log(Level.SEVERE, null, ex);
+//        }
     }
 
     /**
@@ -75,15 +99,20 @@ public class SpectatorEndpoint {
     public void onError(Session session, Throwable error) {
         Logger.getLogger(SpectatorEndpoint.class.getName()).log(Level.SEVERE, "An error occured in session " + session, error);
     }
-    
+
     /**
-     * 
+     *
      * @param session
-     * @param reason 
+     * @param reason
      */
     @OnClose
     public void onClose(Session session, CloseReason reason) {
-        
+        System.out.println("Closing session");
+        for (int i = 0; i < sessions.size(); i++) {
+            if (sessions.get(i) == session) {
+                sessions.remove(i);
+            }
+        }
+        System.out.println("Sessions size: " + sessions.size());
     }
-
 }
