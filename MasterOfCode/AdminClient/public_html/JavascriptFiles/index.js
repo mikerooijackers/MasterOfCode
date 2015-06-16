4e4ea49d5ab716428a4607f19cf96cf5fc21b5e7
Noor van Oekel <noorvanoekel@gmail.com>
2015-06-16 11:28:53 +0200

    Start, stop, freeze, pause of competition and round functionality added in client

--- MasterOfCode/AdminClient/public_html/JavascriptFiles/index.js
+++ MasterOfCode/AdminClient/public_html/JavascriptFiles/index.js
@@ -39,6 +39,14 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
             $scope.MenuUserVisibility = {'display': 'block'};
             $scope.MenuGameVisibility = {'display': 'none'};
 
+            $scope.startCompBtn = document.getElementById("startCompBtn");
+            $scope.stopCompBtn = document.getElementById("stopCompBtn");
+            $scope.startRoundBtn = document.getElementById("startRoundBtn");
+            $scope.pauseRoundBtn = document.getElementById("pauseRoundBtn");
+            $scope.freezeRoundBtn = document.getElementById("freezeRoundBtn");
+            $scope.stopRoundBtn = document.getElementById("stopRoundBtn");
+
+            var isPausedOrFrozen = false;
             var NewSessionConnectionMessage = {
                 MessageType: "NewSessionConnectionMessage",
                 Username: "Jordi"
@@ -55,20 +63,19 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
                 console.log("InformationService: trying to update the participants");
                 for (var u in data.Users) {
                     var user = {};
-                    user.Username = data.Users[u].Username;
                     user.Email = data.Users[u].Email;
                     user.FullName = data.Users[u].FullName;
                     user.Company = data.Users[u].Company;
                     user.TelephoneNumber = data.Users[u].TelephoneNumber;
                     user.Team = data.Users[u].Team;
-                    InformationService.participants[user.Username] = user;
+                    InformationService.participants[user.FullName] = user;
                 }
 
                 console.log("InformationService: participants updated");
             });
 
             $rootScope.$on("GetTeamsRequestMessage", function (event, data) {
-                console.log("InformationService: trying to update the teams")
+                console.log("InformationService: trying to update the teams");
                 for (var t in data.Teams) {
                     var team = {};
                     team.Score = data.Teams[t].Score;
@@ -89,6 +96,26 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
                 console.log("InformationService: teams updated");
             });
 
+            // Methods
+
+            disableAllPlayButtons = function () {
+                $scope.startCompBtn.disabled = true;
+                $scope.stopCompBtn.disabled = true;
+                $scope.startRoundBtn.disabled = true;
+                $scope.pauseRoundBtn.disabled = true;
+                $scope.freezeRoundBtn.disabled = true;
+                $scope.stopRoundBtn.disabled = true;
+            };
+
+            enableAllPlayButtons = function () {
+                $scope.startCompBtn.disabled = false;
+                $scope.stopCompBtn.disabled = false;
+                $scope.startRoundBtn.disabled = false;
+                $scope.pauseRoundBtn.disabled = false;
+                $scope.freezeRoundBtn.disabled = false;
+                $scope.stopRoundBtn.disabled = false;
+            };
+
             // Scope methods
 
             $scope.showCategoryUsers = function () {
@@ -105,6 +132,9 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
             };
 
             $scope.startCompetition = function () {
+                enableAllPlayButtons();
+                $scope.startCompBtn.disabled = true;
+
                 var StartCompetitionRequestMessage = {
                     MessageType: "StartCompetitionRequestMessage"
                 };
@@ -113,6 +143,9 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
             };
 
             $scope.stopCompetition = function () {
+                disableAllPlayButtons();
+                $scope.startCompBtn.disabled = false;
+
                 var StopCompetitionRequestMessage = {
                     MessageType: "StopCompetitionRequestMessage"
                 };
@@ -121,6 +154,10 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
             };
 
             $scope.startRound = function () {
+                enableAllPlayButtons();
+                $scope.startCompBtn.disabled = true;
+                $scope.startRoundBtn.disabled = true;
+
                 var StartRoundRequestMessage = {
                     MessageType: "StartRoundRequestMessage"
                 };
@@ -129,6 +166,10 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
             };
 
             $scope.stopRound = function () {
+                disableAllPlayButtons();
+                $scope.stopCompBtn.disabled = false;
+                $scope.startRoundBtn.disabled = false;
+
                 var StopRoundRequestMessage = {
                     MessageType: "StopRoundRequestMessage"
                 };
@@ -137,19 +178,59 @@ angular.module('adminClient', ['ngRoute', 'ngWebsocket'])
             };
 
             $scope.pauseUnpauseRound = function () {
-                var PauseRoundRequestMessage = {
-                    MessageType: "PauseRoundRequestMessage"
-                };
+                if (isPausedOrFrozen) {
+                    enableAllPlayButtons();
 
-                websocketService.sendMessage(PauseRoundRequestMessage);
+                    var ResumeRequestMessage = {
+                        MessageType: "ResumeRequestMessage"
+                    };
+
+                    websocketService.sendMessage(ResumeRequestMessage);
+
+                    $scope.pauseRoundBtn.innerHTML = "Pause";
+                    isPausedOrFrozen = false;
+                }
+                else {
+                    disableAllPlayButtons();
+                    $scope.pauseRoundBtn.disabled = false;
+
+                    var PauseRoundRequestMessage = {
+                        MessageType: "PauseRoundRequestMessage"
+                    };
+
+                    websocketService.sendMessage(PauseRoundRequestMessage);
+
+                    $scope.pauseRoundBtn.innerHTML = "Unpause";
+                    isPausedOrFrozen = true;
+                }
             };
 
             $scope.freezeUnfreezeRound = function () {
-                var FreezeRoundRequestMessage = {
-                    MessageType: "FreezeRoundRequestMessage"
-                };
+                if (isPausedOrFrozen) {
+                    enableAllPlayButtons();
 
-                websocketService.sendMessage(FreezeRoundRequestMessage);
+                    var UnFreezeRoundRequestMessage = {
+                        MessageType: "UnFreezeRoundRequestMessage"
+                    };
+
+                    websocketService.sendMessage(UnFreezeRoundRequestMessage);
+
+                    $scope.freezeRoundBtn.innerHTML = "Freeze";
+                    isPausedOrFrozen = false;
+                }
+                else {
+                    disableAllPlayButtons();
+                    $scope.freezeRoundBtn.disabled = false;
+
+                    var FreezeRoundRequestMessage = {
+                        MessageType: "FreezeRoundRequestMessage"
+                    };
+
+                    websocketService.sendMessage(FreezeRoundRequestMessage);
+
+                    $scope.freezeRoundBtn.innerHTML = "Unfreeze";
+                    isPausedOrFrozen = true;
+                }
             };
         });
 
